d4db1c9262061fdaaf967fd6d160bae81a1b11ee
Affonso-gois <affonso.dev.gois@gmail.com>
2025-04-01 16:21:44 -0300

    mais um passo concluido

--- scripts.js
+++ scripts.js
@@ -1,20 +1,37 @@
 const minhasecao = document.querySelector(".sobre") //mapeamento da secao sobre 
 
+const projects = document.querySelector(".meusprojetos") //mapeamento secao projetos
 
-const projects = document.querySelector(".meusprojetos")
+const irparatopo = document.querySelector(".caixa-master") // mapeamento ir para topo
+
+const competencia = document.querySelector(".caixa-competencia") // mapeamento competencia
 
 // essa funcao faz rolar até um lugar especifico na tela
-function cliqueiNoBotao(){ 
+function cliqueiNoBotao() {
 
-   minhasecao = document.getElementById("sobreMim").scrollIntoView({
-    behavior: 'smooth'})
+    minhasecao = document.getElementById("sobreMim").scrollIntoView({
+        behavior: 'smooth'
+    })
 
 }
 
-
-function cliqueiNoProjeto(){ 
+function cliqueiNoProjeto() {
 
     projects = document.getElementById("meusProjetos").scrollIntoView({
-     behavior: 'smooth'})
- 
- }
+        behavior: 'smooth'
+    })
+
+}
+
+function cliqueiNaSeta() {
+
+    irparatopo = document.getElementById("caixaMaster").scrollIntoView({
+        behavior: 'smooth'
+    })
+}
+
+function cliqueiNaCompetencia() {
+    competencia = document.getElementById("competencia").scrollIntoView({
+        behavior: 'smooth'
+    })
+}
