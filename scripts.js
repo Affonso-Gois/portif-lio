269160e9f3045a2971ad81dac2c85b19658c66c9
Affonso-gois <affonso.dev.gois@gmail.com>
2025-04-05 14:33:46 -0300

    função scrolltoword

--- scripts.js
+++ scripts.js
@@ -1,13 +1,6 @@
-const minhasecao = document.querySelector(".sobre") //mapeamento da secao sobre 
-
-const projects = document.querySelector(".meusprojetos") //mapeamento secao projetos
-
-const irparatopo = document.querySelector(".caixa-master") // mapeamento ir para topo
-
-const competencia = document.querySelector(".caixa-competencia") // mapeamento competencia
-
-// essa funcao faz rolar até um lugar especifico na tela
 function cliqueiNoBotao() {
+
+    const minhasecao = document.querySelector(".sobre") //mapeamento da secao sobre
 
     minhasecao = document.getElementById("sobreMim").scrollIntoView({
         behavior: 'smooth'
@@ -17,6 +10,8 @@ function cliqueiNoBotao() {
 
 function cliqueiNoProjeto() {
 
+    const projects = document.querySelector(".meusprojetos") //mapeamento secao projetos
+
     projects = document.getElementById("meusProjetos").scrollIntoView({
         behavior: 'smooth'
     })
@@ -25,13 +20,54 @@ function cliqueiNoProjeto() {
 
 function cliqueiNaSeta() {
 
+    const irparatopo = document.querySelector(".caixa-master") // mapeamento ir para topo
+
     irparatopo = document.getElementById("caixaMaster").scrollIntoView({
         behavior: 'smooth'
     })
 }
 
 function cliqueiNaCompetencia() {
+
+    const competencia = document.querySelector(".caixa-competencia") // mapeamento competencia
+
     competencia = document.getElementById("competencia").scrollIntoView({
         behavior: 'smooth'
     })
 }
+
+function scrollToWord() {
+    const searchTerm = document.getElementById("searchInput").value.toLowerCase(); //pega o valor do input e converte para minuscula
+    const elements = document.body.querySelectorAll('*'); //pega todos os elementos do body
+
+    
+
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        if (element.textContent.toLowerCase().includes(searchTerm)) { //verifica se o elemento contém a palavra pesquisada
+            element.scrollIntoView({ behavior: 'smooth', block: 'start' }); //rola a pagina para o elemento
+            return; //para a busca
+        }
+        
+        
+    } 
+
+}
+
+function highlightWord() {
+    const input = document.getElementById("searchInput").value.trim();
+    const paragraph = document.body.querySelectorAll('*');
+    const regex = new RegExp(`(${input})`, 'gi');
+    
+    if (input !== '') {
+        paragraph.innerHTML = paragraph.textContent.replace(regex, '<span class="highlight">$1</span>');
+    }
+}
+
+
+const botao = document.getElementById("meuBotao");
+
+botao.addEventListener("click", highlightWord())
+
+
+
